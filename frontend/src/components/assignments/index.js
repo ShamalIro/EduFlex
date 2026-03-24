7706e3610452e8f1644beca4393273b05eefac58
hasithamilan2 <hasithamilan2@gmail.com>
2026-03-24 11:44:27 +0530

    Add index file for exporting assignment components

---
+++ frontend/src/components/assignments/index.js
@@ -0,0 +1,4 @@
+// Assignment Components Export
+export { AssignmentForm } from './AssignmentForm';
+export { AssignmentCard, AssignmentCardCompact } from './AssignmentCard';
+export { AssignmentList } from './AssignmentList';
