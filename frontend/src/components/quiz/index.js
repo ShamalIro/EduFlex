4763137195dffd0c6b1dabddd560c18885f2559d
hasithamilan2 <hasithamilan2@gmail.com>
2026-03-24 11:44:38 +0530

    Add index file for exporting quiz components

---
+++ frontend/src/components/quiz/index.js
@@ -0,0 +1,5 @@
+// Quiz Components Export
+export { QuizForm } from './QuizForm';
+export { QuestionBuilder, createEmptyQuestion } from './QuestionBuilder';
+export { QuizCard, QuizCardCompact } from './QuizCard';
+export { QuizList } from './QuizList';
